8b29f1cd1385b32ef030724301ba2a09519781ee
Yash Sharma <yashsharma0558@gmail.com>
2023-03-12 21:14:30 +0530

    signup

---
+++ app/src/main/java/com/example/fibo/sp_signup.java
@@ -0,0 +1,14 @@
+package com.example.fibo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sp_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp_signup);
+    }
+}
