a811e95e8ff33b9ded39c54704b22ebd0e03d3ca
Yash Sharma <yashsharma0558@gmail.com>
2023-04-03 03:16:06 +0530

    donate

---
+++ app/src/main/java/com/example/fibo/volunteer_donate.java
@@ -0,0 +1,14 @@
+package com.example.fibo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class volunteer_donate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.volunteer_donate);
+    }
+}
