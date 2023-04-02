c3d9ced290cd31759256b19b645558fba3eb420e
Yash Sharma <yashsharma0558@gmail.com>
2023-04-03 02:42:19 +0530

    connect add

---
+++ app/src/main/java/com/example/fibo/volunteer_connect.java
@@ -0,0 +1,14 @@
+package com.example.fibo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class volunteer_connect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.volunteer_connect);
+    }
+}
